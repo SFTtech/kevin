8a3a794a15727e83ee2b164db9b9935ac6e8ffa2
Jonas Jelten <jj@sft.mx>
2016-03-12 01:33:27 +0100

    dist: release v0.1

--- setup.py
+++ setup.py
@@ -4,12 +4,38 @@ from distutils.core import setup
 
 setup(
     name="kevin",
-    version="1.0",
+    version="0.1",
     description="Self-hostable continuous integration toolkit",
-    author="SFT Technologies",
-    author_email="michael@ensslin.cc",
+    long_description=(
+        "Components for running a continuous integration service "
+        "right on your own servers.\n"
+        "Kevin interacts with the Internet, falk manages the virtual machines "
+        "and containers, chantal executes your build jobs."
+    ),
+    maintainer="SFT Technologies",
+    maintainer_email="jj@stusta.net",
     url="https://github.com/SFTtech/kevin",
-    packages=["kevin",
-              "chantal",
-              "falk"],
+    license='AGPL3+',
+    packages=[
+        "kevin",
+        "kevin.action",
+        "kevin.project",
+        "kevin.service",
+        "kevin.simulator",
+        "chantal",
+        "falk",
+        "falk.vm",
+    ],
+    platforms=[
+        'Linux',
+    ],
+    classifiers=[
+        ("License :: OSI Approved :: "
+         "GNU Affero General Public License v3 or later (AGPLv3+)"),
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Version Control",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
 )
