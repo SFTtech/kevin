357800a2b457583ab5b70c4e3748902b3983486a
kindl <kindl@users.noreply.github.com>
2016-04-10 22:30:28 +0200

    adapted client to new way of speaking with backend

--- mandy/app/routes/projects.js
+++ mandy/app/routes/projects.js
@@ -5,13 +5,19 @@ export default Ember.Route.extend({
     // TODO: dynamically insert the correct socket url
     var socket = this.get('websockets').socketFor('ws://localhost:7777/ws');
     var target = this.store;
+
     socket.on('open', () => {
       console.log("sending proj request");
-      socket.send("gimme projects");
+      socket.send({
+        "method": "list",
+        "collection": "projects"
+      }, true);
     }, this);
+
     socket.on('message', (event) => {
       // fill the store with the data
       var update = JSON.parse(event.data);
+
       target.push(update);
     }, this);
   },
