d25ae9b4f07369d49be4019c71ac229db94bedef
Jonas Jelten <jj@sft.mx>
2017-04-22 18:00:47 +0200

    kevin: adaptive logging verbosity for the simulator

--- kevin/simulator/__main__.py
+++ kevin/simulator/__main__.py
@@ -8,8 +8,10 @@ then waits for interaction.
 """
 
 import argparse
+import asyncio
 
 from . import github
+from ..util import log_setup
 
 
 def main():
@@ -28,6 +30,12 @@ def main():
     cmd.add_argument("--local-repo-address", default="10.0.2.2",
                      help=("the vm can reach this simulator "
                            "under the given address."))
+    cmd.add_argument("-d", "--debug", action="store_true",
+                     help="enable asyncio debugging")
+    cmd.add_argument("-v", "--verbose", action="count", default=0,
+                     help="increase program verbosity")
+    cmd.add_argument("-q", "--quiet", action="count", default=0,
+                     help="decrease program verbosity")
 
     sp = cmd.add_subparsers(dest="module")
 
@@ -35,6 +43,13 @@ def main():
     github.GitHub.argparser(sp)
 
     args = cmd.parse_args()
+
+    # set up log level
+    log_setup(args.verbose - args.quiet)
+
+    # enable asyncio debugging
+    loop = asyncio.get_event_loop()
+    loop.set_debug(args.debug)
 
     if args.module is None:
         cmd.print_help()
