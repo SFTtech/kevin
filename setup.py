b22fe57a5ebb6bbfdf1c9945a8e9c28a79700a58
Jonas Jelten <jj@sft.mx>
2018-07-29 01:48:21 +0200

    release v0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import glob
 
 setup(
     name="kevin",
-    version="0.4",
+    version="0.5",
     description="Self-hostable continuous integration toolkit",
     long_description=(
         "Components for running a continuous integration service "
