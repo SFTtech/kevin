e6dae0b7ec56238968b3c7d7b657948ae1459b12
Jonas Jelten <jj@sft.mx>
2020-05-09 19:57:20 +0200

    kevin: force-rebuild support

--- kevin/build_manager.py
+++ kevin/build_manager.py
@@ -14,27 +14,36 @@ class BuildManager:
         # stores known builds by (project, hash) -> build
         self.builds = dict()
 
-    async def new_build(self, project, commit_hash, create_new=True):
+    async def new_build(self, project, commit_hash, create_new=True,
+                        force_rebuild=False):
         """
         Create a new build or return it from the cache.
         """
         cache_key = (project, commit_hash)
 
-        if cache_key in self.builds:
-            # already known build.
-            return self.builds[cache_key]
+        cached_build = self.builds.get(cache_key)
+        if cached_build and not force_rebuild:
+            return cached_build
 
         else:
             # this tries loading from filesystem
             newbuild = Build(project, commit_hash)
 
-            # try loading from filesystem
-            await newbuild.load_from_fs()
+            if force_rebuild:
+                if cached_build:
+                    del self.builds[cache_key]
+            else:
+                # try loading from filesystem
+                await newbuild.load_from_fs()
 
-            # store it as known build?
+            # store build to the cache
+            # newbuild.completed => it could be loaded from fs
+            # create_new => as its a new build, always store it
             if newbuild.completed or create_new:
                 self.builds[cache_key] = newbuild
             else:
+                # if the build couldn't be loaded from fs
+                # and it's not a new build
                 newbuild = None
 
             return newbuild
